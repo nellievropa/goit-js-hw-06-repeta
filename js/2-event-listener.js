4c4280e9cb3ae48f99bcd61fd21586b3f7d0aad6
nellievropa <nellievropa23@gmail.com>
2023-06-26 17:12:25 +0300

    чуть-чуть

--- js/2-event-listener.js
+++ js/2-event-listener.js
@@ -59,24 +59,127 @@ function onTargetBTNClick () {
 
 //  і тоді попередній код може бути записаний так( передаємо посилання на функцію)
 // у цих функцій обов'язково є event -перший параметр () прибираємо
-addListenerBtn.addEventListener('click', event => {
-        console.log(event);
-        console.log('Вішаю слухача події на цільову кнопку');
+// addListenerBtn.addEventListener('click', event => {
+//         console.log(event);
+//         console.log('Вішаю слухача події на цільову кнопку');
     
-        targetBtn.addEventListener('click', onTargetBTNClick);
-    });
+//         targetBtn.addEventListener('click', onTargetBTNClick);
+//     });
 
 
-removeListenerBtn.addEventListener('click', event => {
-    console.log(event);
-        console.log('Знімаю слухача події з цільової кнопки');
+// removeListenerBtn.addEventListener('click', event => {
+//     console.log(event);
+//         console.log('Знімаю слухача події з цільової кнопки');
     
-        targetBtn.removeEventListener('click', onTargetBTNClick);
-    });
+//         targetBtn.removeEventListener('click', onTargetBTNClick);
+//     });
 
 
 
-function logMessage(event) {
-    console.log(event);
-    console.log('Click on button');
-}
+// function logMessage(event) {
+//     console.log(event);
+//     console.log('Click on button');
+// }
+
+// Приклад квадратик, що рухається
+
+// const button = document.querySelector('.js-click');
+// const container = document.querySelector('.js-container');
+// button.addEventListener('click', onClick);
+// ми можемо додати клик на любий об'єкт, наприклад-на квадратик і він буде рухатись при кліку по ньому
+// для цього  88 строку змінюємо і addEventListener додаєм на контайнер
+
+// container.addEventListener('click', onClick1);
+
+// let step = 0;
+
+// function onClick1(event) {
+//     step += 5;
+//     container.style.marginLeft = `${step}px`;
+//     // якщо хочемо щоб квадратик рухався по діагоналі додаєм просто
+//     container.style.marginTop = `${step}px`;
+
+//     console.log(event.currentTarget);
+// }
+
+// currentTarget = наш елемент
+
+
+// <!-- по кліку на 3 крапки читач переходить до всього тексту -->
+   
+    // const title = document.querySelector('.js-title');
+    // const MaxLength = 13;
+    // довжина з крапками 
+    // const totalLength = MaxLength + 3;
+// ми хочемо, щоб п кліку текст обрізався, а потім був виден весь
+// тому додаємо слухача
+// title.addEventListener('click', onClick);
+
+// function onClick(event) {
+    
+    // event.currentTarget винесемо в змінну
+    // const innerTitle = event.currentTarget;
+    // const str = innerTitle.textContent.slice(0,MaxLength);
+// тепер пишему умову коли, що ми будемо бачити
+// if(innerTitle.textContent.length > totalLength) {
+   // і замінюємо event.currentTarget всюди , де використовуємо на innerTitle
+//    const str = innerTitle.textContent.slice(0,MaxLength);
+//    const remainder = innerTitle.textContent.slice(MaxLength);
+   // щоб додати залишок до data atribute звертаємось до innerTitle
+//    innerTitle.setAttribute('data-title', remainder);
+   // обрізаємо титул в HTML і додаємо ... -додаэмо змінну MaxLength в початок
+//    innerTitle.textContent = str+'...';
+// } else {
+    // отримати кінець
+    // const  remainder = innerTitle.dataset.title;
+    // взяти початок і додати до нього кінець, який тільки но створили
+    // innerTitle.textContent = str + remainder;
+    // getAttribute
+//     console.log(remainder);
+// }
+
+
+// }
+
+// якщо ми обробляєм елемент, на який клікаємо, то треба використовувати event.current
+// щоб максимально абстрагуватися від HTML
+// тому  const str = title.textContent.slice(0,13); заміняємо на
+
+    // треба обрізати текст Н1- тбто ми його модифікуємо slice працює і з РЯДКАМИ!!!
+    // const str = title.textContent.slice(0,13);
+    //  все те, що вирізали буде тут
+// const remainder = title.currentTarget.textContent.slice(13);
+//     console.log(str);
+//     console.log(remainder);
+
+// Приклад коли багато подібних абзаців, а змінні названі правильно і нічого міняти не треба!
+// бо код працює!
+
+const title = document.querySelector('.js-title');
+const title1 = document.querySelector('.js-title1');
+const title2 = document.querySelector('.js-title2');
+const MaxLength = 13;
+const totalLength = MaxLength + 3;
+title.addEventListener('click', onClick);
+title1.addEventListener('click', onClick);
+title2.addEventListener('click', onClick);
+
+function onClick(event) {
+    const innerTitle = event.currentTarget;
+    const str = innerTitle.textContent.slice(0,MaxLength);
+
+    if(innerTitle.textContent.length > totalLength) {
+        const remainder = innerTitle.textContent.slice(MaxLength);
+        innerTitle.setAttribute('data-title', remainder);
+        innerTitle.textContent = str+'...';
+    } else {
+// якщо рядок коротший за 13 символів
+        const  remainder = innerTitle.dataset.title;
+        if (remainder) {
+            innerTitle.textContent = str + remainder;
+            console.log(remainder);
+        }
+      
+        }              
+};
+// обов'язково використовуємо event and currentTarget!!!
--- js/23-input-enents.js
+++ js/23-input-enents.js
@@ -19,39 +19,46 @@ const refs = {
 
 // перша подия input
 
-// refs.input.addEventListener('focus', onInputFocus);
-// refs.input.addEventListener('blur', onInputBlur);
+refs.input.addEventListener('focus', onInputFocus);
+
+function onInputFocus() {
+    console.log('input отримав фокус - подія focus');
+    };
+
+refs.input.addEventListener('blur', onInputBlur);
+function onInputBlur() {
+    console.log('input загубив фокус - подія blur');
+};
 
 // повісили слухателя на зміни
 // refs.input.addEventListener('change', onInputChange);
+// function onInputChange(event) {
+//     // з цього ми можемо отримати значееня инпута
+// console.log(event.currentTarget.value);
+// refs.nameLable.textContent = event.currentTarget.value;
+// кнопка повинна бути активною при чекнутому чекбоксі!
+// використовуємо подію чендж
 
+// }
 // стандартна подія input
 // refs.input.addEventListener('input', onInputChange);
 
 // щоб виконати нашу задачу: щоб змінювалось значення спану!
 refs.input.addEventListener('input', onInputChange);
 
-
-refs.licenseCheckbox.addEventListener('change', onLicenseChange)
-
-function onInputFocus() {
-console.log('input отримав фокус - подія focus');
-};
-
-function onInputBlur() {
-    console.log('input загубив фокус - подія blur');
-};
-
 function onInputChange(event) {
-    // з цього ми можемо отримати значееня инпута
 console.log(event.currentTarget.value);
 refs.nameLable.textContent = event.currentTarget.value;
-// кнопка повинна бути активною при чекнутому чекбоксі!
-// використовуємо подію чендж
-
 }
+// span имеет только textContent!!!!
+
+// кнопка повинна активуватись тільки коли чекнутий чебокс
+// для чекбоксів і радіобатонів використовується подія 'change'
+refs.licenseCheckbox.addEventListener('change', onLicenseChange)
 
 function onLicenseChange(event) {
-    console.log(event.currentTarget.checked);
-    refs.b
+    console.log(event.currentTarget.checked);  
+    console.log('refs.disabled:', refs.btn.disabled);
+//  кнопка вимкнена     коли чекбокс НЕ активна
+      refs.btn.disabled = !event.currentTarget.checked;
 }
