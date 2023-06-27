a891e7680dd81a5dd0a78102cfe0d4eff60c4268
nellievropa <nellievropa23@gmail.com>
2023-06-27 17:14:00 +0300

    +6+7

--- js/26-modai.js
+++ js/26-modai.js
@@ -71,4 +71,36 @@ if(event.code === ESC_KEY_CODE) {
 
 };
 
+// 6
+const inputContent = document.querySelector('#validation-input');
+const maxLength = 6;
+// const maxLength =document.querySelector('input[data-length="6"]');
+inputContent.addEventListener('blur', onInputBlur);
+
+function onInputBlur(event) {
+
+    // console.log(event.currentTarget);
+    const content = event.currentTarget;
+
+ if(content.value.length === maxLength) {
+    content.classList.add('valid');
+ }else{
+content.classList.add('invalid');
+ }
+}
+
+// 7
+const inputFont = document.querySelector('#font-size-control');
+console.log(inputFont.value)
+const spanSize = document.querySelector('#text');
+console.log(spanSize.style.fontSize);
+
+inputFont.addEventListener('input', onFontSiseControl);
+
+function onFontSiseControl(event) {
+// console.log(event.currentTarget.value);
+spanSize.style.fontSize = event.currentTarget.value + 'px';
+}
+
+
 
