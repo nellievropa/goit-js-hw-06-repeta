90fb1a6cd7ff0873d656451ff719700119ca3be9
nellievropa <nellievropa23@gmail.com>
2023-06-30 15:51:29 +0300

    нічого

--- js/26-modai.js
+++ js/26-modai.js
@@ -71,23 +71,7 @@ if(event.code === ESC_KEY_CODE) {
 
 };
 
-// 6
-const inputContent = document.querySelector('#validation-input');
-const maxLength = 6;
-// const maxLength =document.querySelector('input[data-length="6"]');
-inputContent.addEventListener('blur', onInputBlur);
 
-function onInputBlur(event) {
-
-    // console.log(event.currentTarget);
-    const content = event.currentTarget;
-
- if(content.value.length === maxLength) {
-    content.classList.add('valid');
- }else{
-content.classList.add('invalid');
- }
-}
 
 // 7
 const inputFont = document.querySelector('#font-size-control');
