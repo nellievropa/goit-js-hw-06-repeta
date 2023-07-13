8bc386635fa96e1b3e157547985b9262d92f7d4b
nellievropa <nellievropa23@gmail.com>
2023-07-13 16:38:02 +0300

    car 07

--- js/07-car-modal.js
+++ js/07-car-modal.js
@@ -1,4 +1,6 @@
-const cars =[
+//Завдання : клікнути на картку і отримати модальне вікно з повною інформацією
+
+const cars = [
     {
         id: 1,
         car: "Honda",
@@ -36,3 +38,36 @@ const cars =[
         img: "https://cdn.riastatic.com/photosnew/auto/photo/volvo_fh-13__456672556f.webp",
     },
 ];
+
+
+const container = document.querySelector('.js-container');
+
+// переберем наш масив і створимо новий із Ли  з img: і  car: 
+// щоб мати змогу найти наш елемент нам потрібен ще   id: з дата значеннм
+const markup = cars.map(
+    ({ img, car, id }) => ` <li data-car-id="${id}" class="js-target" class="js-card">
+<img class="js-target" src="${img}" 
+alt="${car}"
+wigth="200">
+<h2 class="js-target">${car}</h2>
+</li>`
+);
+// щоб мати можливість відслідковувати елемент, задаємо клас і вішаємо його на всіх дітей Ли , щоб отримати результат- клік по Лі
+// додамо нашу розмітку в контайнер
+
+container.insertAdjacentHTML('beforeend', markup.join(''))
+// додаємо слухача подій
+container.addEventListener('click', onClick);
+
+function onClick(event) {
+const {target} = event;
+    if(!target.classList.contains("js-target")) {
+        return;
+    }
+    // console.log(event.target);
+    // впевнились, що клікаємо на картку
+// метод клосест дає нам першого спільного батька зі спільним классом
+// тому ми задаємо нашу картку через такі умови
+    const carId = target.dataset.carId ?? target.closest('.js-card').dataset.carId;
+    console.log(carId);
+}
