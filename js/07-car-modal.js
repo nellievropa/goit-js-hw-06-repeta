850206d42ff269e593c389a989b5391506841ad9
nellievropa <nellievropa23@gmail.com>
2023-07-13 14:52:21 +0300

    07 Homework

---
+++ js/07-car-modal.js
@@ -0,0 +1,38 @@
+const cars =[
+    {
+        id: 1,
+        car: "Honda",
+        type: "Civic",
+        price: 12000,
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaaaHQzbknX9_YwDn6Qkun4lhUMwSOeXUKHuify8GNzulgTk2OV5X67aumpLrp7EzHmIw&usqp=CAU",
+    },
+    {
+        id: 2,
+        car: "Audi",
+        type: "Q7",
+        price: 40000,
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSebDnSDztoYJ2wWgWFp95vflTFFDCmM3vJADwfkr-B&s",
+    },
+    {
+        id: 33,
+        car: "BMV",
+        type: "5 series",
+        price: 9000,
+        img: "https://cdn1.riastatic.com/photosnew/auto/photo/bmw_x7__406732716fx.jpg",
+    },
+    {
+        id: 3,
+        car: "Honda",
+        type: "Accord",
+        price: 20000,
+        number: "+380000000000",
+        img: "https://cdn.riastatic.com/photosnew/auto/photo/honda_accord__463602148f.webp",
+    },
+    {
+        id: 4,
+        car: "Volvo",
+        type: "XC60",
+        price: 7000,
+        img: "https://cdn.riastatic.com/photosnew/auto/photo/volvo_fh-13__456672556f.webp",
+    },
+];
